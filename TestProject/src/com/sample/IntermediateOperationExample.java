d47cb57978e2831a3b11cdf62014026d78d132ba
dhaivapriya-backend <dhaivapriya2019r@gmail.com>
2026-04-22 20:34:59 +0530

    second commit

---
+++ TestProject/src/com/sample/IntermediateOperationExample.java
@@ -0,0 +1,18 @@
+package com.sample;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IntermediateOperationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> names=Arrays.asList("Java", "Hibernate", "Oracle", "DBA", "Spring");
+		names.stream().filter(name->name.startsWith("H"))
+		.map(name->name.toUpperCase())
+		.distinct()
+		.sorted()
+		.forEach(System.out::println);
+	}
+
+}
