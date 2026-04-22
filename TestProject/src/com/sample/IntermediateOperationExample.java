806e658316e963d2fd6ee6c0fc00cb2eaa8e03e7
dhaivapriya-backend <dhaivapriya2019r@gmail.com>
2026-04-22 20:39:40 +0530

    Stream API commit

--- TestProject/src/com/sample/IntermediateOperationExample.java
+++ TestProject/src/com/sample/IntermediateOperationExample.java
@@ -7,12 +7,9 @@ public class IntermediateOperationExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<String> names=Arrays.asList("Java", "Hibernate", "Oracle", "DBA", "Spring");
-		names.stream().filter(name->name.startsWith("H"))
-		.map(name->name.toUpperCase())
-		.distinct()
-		.sorted()
-		.forEach(System.out::println);
+		List<String> names = Arrays.asList("Java", "Hibernate", "Oracle", "DBA", "Spring");
+		names.stream().filter(name -> name.startsWith("H")).map(name -> name.toUpperCase()).distinct().sorted()
+				.forEach(System.out::println);
 	}
 
 }
