c848bec6447fad0559b300e24d7b76aceeb220fd
dhaivapriya-backend <dhaivapriya2019r@gmail.com>
2026-04-24 13:05:06 +0530

    commit Addition.java

---
+++ seccond-java-project/src/com/aaa/Addition.java
@@ -0,0 +1,15 @@
+package com.aaa;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=3;
+		int b=4;
+		int c=a+b;
+		System.out.println("c="+c);
+
+	}
+
+}
