07a10ef1c379aea9f5696772700b67ac4f383642
dhaivapriya-backend <dhaivapriya2019r@gmail.com>
2026-04-24 14:53:18 +0530

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of
    https://github.com/dhaivapriya-backend/first-java-project.git into main

--- seccond-java-project/src/com/aaa/Addition.java
+++ seccond-java-project/src/com/aaa/Addition.java
@@ -2,13 +2,23 @@ package com.aaa;
 
 public class Addition {
 
+	int a, b;
+
+	public Addition(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	void display() {
+		System.out.println(a + b);
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int a=3;
-		int b=4;
-		int c=a+b;
-		System.out.println("c="+c);
+
+		Addition c = new Addition(2, 3);
+		c.display();
 
 	}
 
