5aae381ab5278a635ff695b407fc7777f87fb70c
dhaivapriya-backend <dhaivapriya2019r@gmail.com>
2026-04-25 15:18:30 +0530

    Update Addition.java
    Using Scanner Addition of Two Number.

--- seccond-java-project/src/com/aaa/Addition.java
+++ seccond-java-project/src/com/aaa/Addition.java
@@ -1,15 +1,21 @@
 package com.aaa;
-
+import java.util.Scanner;
 public class Addition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a=3;
-		int b=4;
-		int c=5;
-		int d=a+b+c;
-		System.out.println("d="+d);
+		System.out.println("Adition of 3 Numbers:");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the first Number:");
+		int a=sc.nextInt();
+
+		System.out.println("Enter the second Number:");
+		int b=sc.nextInt();
+
+		int c=a+b;
+
+		System.out.println("Addition of Two Number:"+c);
 
 	}
 
